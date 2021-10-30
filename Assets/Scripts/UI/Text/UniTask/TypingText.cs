6e064bceeff67ee495c68889757ac9b594d9f465
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-10-30 19:59:14 +0700

    added TypingText feature based on UniTask

---
+++ Assets/Scripts/UI/Text/UniTask/TypingText.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Template.UI.Text.UniTask {
+	public class TypingText : BaseTypingText {
+		CancellationTokenSource _cancellationTokenSource;
+
+		public event Action OnEnd;
+
+		void Start()
+		{
+			Init("Hello world, It's UniTask implementation of TypingText!");
+			Launch();
+			OnEnd += () => { Debug.Log("UniTasksOnEnd"); };
+		}
+		
+		
+		public override void Launch() 
+		{
+			_cancellationTokenSource = new CancellationTokenSource();
+			StartTypingText().ToUniTask(PlayerLoopTiming.Update, _cancellationTokenSource.Token);
+		}
+
+		public override void Break() 
+		{
+			base.Break();
+			_cancellationTokenSource?.Cancel();
+			OnEnd?.Invoke();
+		}
+
+		protected override void Reset() 
+		{
+			base.Reset();
+			_cancellationTokenSource?.Cancel();
+		}
+	}
+}
